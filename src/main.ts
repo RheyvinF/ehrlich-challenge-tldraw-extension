2d0b33d28f8f0b3d50fcb5a31ae1788ed9135b87
Rheyvin Fernandez <rhyferzfh@gmail.com>
2023-10-01 12:01:38 +0800

    feat: add websessions to persist data on restart

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,46 @@
 let extTab: ext.tabs.Tab | null = null;
 let extWindow: ext.windows.Window | null = null;
 let extWebview: ext.webviews.Webview | null = null;
+let extWebsession: ext.websessions.Websession | null = null;
 
 ext.runtime.onExtensionClick.addListener(async () => {
+  try {
+    if (extTab) return;
 
-  if (extTab) return;
-
-  extTab = await ext.tabs.create({
-    text: 'TLDraw'
-  });
-
-  extWindow = await ext.windows.create();
-
-  const windowSize: ext.windows.Size = await ext.windows.getContentSize(extWindow.id);
-
-  extWebview = await ext.webviews.create({
-    window: extWindow,
-    bounds: {
-      x: 0,
-      y: 0,
-      width: windowSize.width,
-      height: windowSize.height
-    },
-    autoResize: {
-      width: true,
-      height: true
-    }
-  });
-
-  await ext.webviews.loadURL(extWebview.id, 'https://www.tldraw.com/');
+    extTab = await ext.tabs.create({
+      text: 'TLDraw'
+    });
+  
+    extWebsession = await ext.websessions.create({ 
+      partition: 'Test',
+      persistent: true,
+      global: false,
+      cache: true,
+    });
+  
+    extWindow = await ext.windows.create();
+  
+    const windowSize: ext.windows.Size = await ext.windows.getContentSize(extWindow.id);
+  
+    extWebview = await ext.webviews.create({
+      window: extWindow,
+      websession: extWebsession,
+      bounds: {
+        x: 0,
+        y: 0,
+        width: windowSize.width,
+        height: windowSize.height
+      },
+      autoResize: {
+        width: true,
+        height: true
+      }
+    });
+  
+    await ext.webviews.loadURL(extWebview.id, 'https://www.tldraw.com/');
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 ext.tabs.onClickedClose.addListener(async () => {
